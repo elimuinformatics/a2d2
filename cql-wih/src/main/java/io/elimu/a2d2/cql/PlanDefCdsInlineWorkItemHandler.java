5fd9ea6d590e264e1fd3766922f1663ab080e08b
Mariano De Maio <marianbuenosayres@gmail.com>
2022-07-04 15:01:02 -0300

    Added initial test

--- cql-wih/src/main/java/io/elimu/a2d2/cql/PlanDefCdsInlineWorkItemHandler.java
+++ cql-wih/src/main/java/io/elimu/a2d2/cql/PlanDefCdsInlineWorkItemHandler.java
@@ -6,13 +6,14 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-import org.apache.commons.codec.binary.Base64;
 import org.cqframework.cql.cql2elm.CqlTranslator;
 import org.cqframework.cql.cql2elm.LibraryManager;
 import org.cqframework.cql.cql2elm.ModelManager;
 import org.cqframework.cql.elm.execution.Library;
+import org.hl7.fhir.r4.model.Attachment;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.PlanDefinition;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItemManager;
@@ -26,9 +27,6 @@ import org.opencds.cqf.cql.engine.model.ModelResolver;
 
 import com.google.gson.Gson;
 
-import org.hl7.fhir.r4.model.Attachment;
-import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.PlanDefinition;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.SimpleRequestHeaderInterceptor;
@@ -57,6 +55,9 @@ public class PlanDefCdsInlineWorkItemHandler implements WorkItemHandler {
 			}
 		}
 		IGenericClient fhirClient = FhirContext.forR4().newRestfulGenericClient(fhirServerUrl);
+		if (fhirServerAuth != null && !"".equals(fhirServerAuth.trim())) {
+			fhirClient.registerInterceptor(new SimpleRequestHeaderInterceptor("Authorization", fhirServerAuth));
+		}
 		PlanDefinition planDefinition = null;
 		if (planDefId != null) {
 			planDefinition = fhirClient.fetchResourceFromUrl(PlanDefinition.class, fhirServerUrl + "/PlanDefinition/" + planDefId);
@@ -70,33 +71,51 @@ public class PlanDefCdsInlineWorkItemHandler implements WorkItemHandler {
 			}
 			planDefinition = (PlanDefinition) bundle.getEntryFirstRep().getResource();
 		}
-		Bundle bundle2 = fhirClient.fetchResourceFromUrl(Bundle.class, fhirServerUrl + "/Library");
-		List<org.hl7.fhir.r4.model.Library> r4libraries = bundle2.getEntry().stream().map(e -> { return (org.hl7.fhir.r4.model.Library) e.getResource(); }).collect(Collectors.toList());
+		org.hl7.fhir.r4.model.Library r4library = fhirClient.fetchResourceFromUrl(org.hl7.fhir.r4.model.Library.class, fhirServerUrl + "/Library/" + planDefinition.getIdElement().getIdPart());
 		List<Library> libraries = new LinkedList<>();
+		/*List<org.hl7.fhir.r4.model.Library> r4libraries = bundle2.getEntry().stream().map(e -> { return (org.hl7.fhir.r4.model.Library) e.getResource(); }).collect(Collectors.toList());
+		Libary mainLibrary = 
 		ModelManager modelManager = new ModelManager();
 		LibraryManager libraryManager = new LibraryManager(modelManager);
 		for (org.hl7.fhir.r4.model.Library library : r4libraries) {
 			for (Attachment content : library.getContent()) {
 				if ("text/cql".equals(content.getContentType())) {
 					try {
-						byte[] base64Data = content.getData();
-						String cql = new String(Base64.decodeBase64(base64Data));
+						String cql = new String(content.getData());
 						CqlTranslator translator = CqlTranslator.fromText(cql, modelManager, libraryManager);
-						libraries.add(JsonCqlLibraryReader.read(new StringReader(translator.toJxson())));
+						Library lib = JsonCqlLibraryReader.read(new StringReader(translator.toJxson()));
+						libraries.add(lib);
 					} catch (IOException e) {
 						//TODO error
+						e.printStackTrace();
 					}
+				}
+			}
+		}*/
+		ModelManager modelManager = new ModelManager();
+		LibraryManager libraryManager = new LibraryManager(modelManager);
+		for (Attachment content : r4library.getContent()) {
+			if ("text/cql".equals(content.getContentType())) {
+				try {
+					String cql = new String(content.getData());
+					CqlTranslator translator = CqlTranslator.fromText(cql, modelManager, libraryManager);
+					Library lib = JsonCqlLibraryReader.read(new StringReader(translator.toJxson()));
+					libraries.add(lib);
+				} catch (IOException e) {
+					//TODO error
+					e.printStackTrace();
 				}
 			}
 		}
 		Context context = new Context(libraries.get(0));
-		context.registerLibraryLoader(new InMemoryLibraryLoader(libraries));
-		if (fhirServerAuth != null && !"".equals(fhirServerAuth.trim())) {
-			fhirClient.registerInterceptor(new SimpleRequestHeaderInterceptor("Authorization", fhirServerAuth));
+		for (Map.Entry<String, Object> entry : prefetchData.entrySet()) {
+			context.setContextValue(entry.getKey(), entry.getValue());
 		}
+		context.registerLibraryLoader(new InMemoryLibraryLoader(libraries));
 		ModelResolver resolver = new R4FhirModelResolver();
 		R4HookEvaluator evaluator = new R4HookEvaluator(resolver);
 		List<CdsCard> cards = evaluator.evaluateCdsHooksPlanDefinition(context, planDefinition, patientId, fhirClient);
+		results.put("cards", cards);
 		results.put("cardsJson", new Gson().toJson(cards));
 		manager.completeWorkItem(workItem.getId(), results);
 	}
--- cql-wih/src/test/java/io/elimu/a2d2/cql/CqfInlineWIHTest.java
+++ cql-wih/src/test/java/io/elimu/a2d2/cql/CqfInlineWIHTest.java
@@ -1,5 +1,49 @@
 package io.elimu.a2d2.cql;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import org.drools.core.process.instance.impl.WorkItemImpl;
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.Enumerations.AdministrativeGender;
+import org.hl7.fhir.r4.model.Identifier.IdentifierUse;
+import org.hl7.fhir.r4.model.Patient;
+import org.junit.Assert;
+import org.junit.Test;
+import org.opencds.cqf.cds.response.CdsCard;
+
 public class CqfInlineWIHTest {
 
+	@Test
+	public void testInlineCall() throws Exception {
+		PlanDefCdsInlineWorkItemHandler handler = new PlanDefCdsInlineWorkItemHandler();
+		WorkItemImpl workItem = new WorkItemImpl();
+		workItem.setParameter("fhirServerUrl", "https://cqf-internal.elimuinformatics.com/fhir");
+		workItem.setParameter("planDefinitionId", "ColorectalCancerScreeningCDS");
+		workItem.setParameter("patientId", "should-screen-ccs");
+		Patient patient = new Patient();
+		patient.setId("should-screen-ccs");
+		patient.addExtension().setUrl("http://hl7.org/fhir/us/core/StructureDefinition/us-core-race").addExtension().setUrl("obmCategory").
+			setValue(new Coding().setSystem("urn:oid:2.16.840.1.113883.6.238").setCode("2028-9").setDisplay("Asian"));
+		patient.addExtension().setUrl("http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity").addExtension().setUrl("obmCategory").
+			setValue(new Coding().setSystem("urn:oid:2.16.840.1.113883.6.238").setCode("2135-2").setDisplay("Hispanic or Latino"));
+		CodeableConcept mrn = new CodeableConcept();
+		mrn.addCoding().setSystem("http://terminology.hl7.org/CodeSystem/v2-0203").setCode("MR").setDisplay("Medical Record Number");
+		patient.addIdentifier().setUse(IdentifierUse.USUAL).setType(mrn).setSystem("http://hospital.smarthealthit.org").setValue("999999992");
+		patient.addName().setFamily("Dere").addGiven("Ben");
+		patient.setGender(AdministrativeGender.MALE);
+		patient.setBirthDate(new SimpleDateFormat("yyyy-MM-dd").parse("1968-01-01"));
+		workItem.setParameter("prefetch_patient", patient);
+		NoOpWorkItemManager manager = new NoOpWorkItemManager();
+		handler.executeWorkItem(workItem, manager);
+		Assert.assertEquals(true, manager.isCompleted());
+		Assert.assertNotNull(workItem.getResults());
+		Assert.assertNotNull(workItem.getResult("cardsJson"));
+		Assert.assertNotNull(workItem.getResult("cards"));
+		List<CdsCard> cards = (List<CdsCard>) workItem.getResult("cards");
+		Assert.assertNotNull(cards);
+		Assert.assertTrue(cards.size() > 0 );
+		
+	}
 }
