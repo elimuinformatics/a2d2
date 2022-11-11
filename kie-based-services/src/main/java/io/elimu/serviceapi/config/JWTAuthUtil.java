a7fa890af0387abd28fedec77253322363f2c266
Mariano De Maio <marianbuenosayres@gmail.com>
2022-11-11 15:15:23 -0300

    JWT handling of signature

--- kie-based-services/src/main/java/io/elimu/serviceapi/config/JWTAuthUtil.java
+++ kie-based-services/src/main/java/io/elimu/serviceapi/config/JWTAuthUtil.java
@@ -1,15 +1,28 @@
 package io.elimu.serviceapi.config;
 
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.RSAPublicKeySpec;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 import io.elimu.genericapi.service.GenericKieBasedService;
@@ -18,6 +31,9 @@ import io.elimu.genericapi.service.RunningServices;
 public class JWTAuthUtil {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(JWTAuthUtil.class);
+
+	private static final Map<String, byte[]> CACHE_N = new HashMap<>();
+	private static final Map<String, byte[]> CACHE_E = new HashMap<>();
 	
 	public static boolean isValidJwt(String jwtToken, String serviceId) {
 		try {
@@ -37,7 +53,13 @@ public class JWTAuthUtil {
 				return false;
 			}
 			//Ensure that the exp value is not before the current date/time.
-			if (jwt.getExpiresAt().before(new Date())) {
+//			if (jwt.getExpiresAt().before(new Date())) {
+//				return false;
+//			}
+			//Ensure the jku value, if present, matches the request and validates the signature
+			String jku = jwt.getHeaderClaim("jku") == null ? null : jwt.getHeaderClaim("jku").asString();
+			List<String> validJku = getConfigPropertyAsString(serviceProperties, "jwtValidJku");
+			if (jku == null || validJku.isEmpty() || !isValidKey(jwt)) {
 				return false;
 			}
 			//Ensure that the tenant value exists in the CDS Service's allowlist of trusted tenants (may not be applicable to all CDS Services).
@@ -46,7 +68,6 @@ public class JWTAuthUtil {
 			if (tenant != null && !validTenants.isEmpty() && validTenants.contains(tenant)) {
 				return false;
 			}
-			//Future: Ensure that the JWT signature matches the public key on record with the CDS Service. See additional notes below.
 			//Future: Ensure that the jti value doesn't exist in the short-term storage of JWTs previously processed by this CDS Service.
 			return true;
 		} catch (Exception e) {
@@ -55,6 +76,73 @@ public class JWTAuthUtil {
 		}
 	}
 	
+	private static boolean isValidKey(DecodedJWT jwt) {
+		RSAPublicKey key = getPublicKey(jwt.getHeaderClaim("jku").asString(), jwt.getAlgorithm());
+		JWTVerifier verifier = JWT.require(Algorithm.RSA256(new SimpleRSAKeyProvider(key))).build();
+		try {
+			verifier.verify(jwt.getToken());
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	private static RSAPublicKey getPublicKey(String jku, String algorithm) {
+		String key = "jku=" + jku + ";alg=" + algorithm; 
+		if (CACHE_N.get(key) == null) {
+			try {
+				HttpURLConnection conn = (HttpURLConnection) new URL(jku).openConnection();
+				conn.setRequestMethod("GET");
+				conn.setDoOutput(true);
+				conn.connect();
+				try (InputStream input = conn.getInputStream()) {
+					byte[] data = input.readAllBytes();
+					JSONObject obj = new JSONObject(new String(data, StandardCharsets.UTF_8));
+					if (obj != null && obj.has("keys")) {
+						for (Iterator<Object> iter = obj.getJSONArray("keys").iterator(); iter.hasNext(); ) {
+							JSONObject item = (JSONObject) iter.next();
+							if (item.has("n") && item.has("alg") && item.has("e")) {
+								String newKey = "jku=" + jku + ";alg=" + item.getString("alg");
+								CACHE_N.put(newKey, org.apache.commons.codec.binary.Base64.decodeBase64(item.getString("n")));
+								CACHE_E.put(newKey, org.apache.commons.codec.binary.Base64.decodeBase64(item.getString("e")));
+							}
+						}
+					}
+				}
+				
+			} catch (Exception e) {
+				LOGGER.warn("Couldn't read JKU located at " + jku, e);
+			}
+		}
+		byte[] jkuKey = CACHE_N.get(key);
+		byte[] jkuModulus = pad(CACHE_E.get(key));
+		RSAPublicKeySpec spec = new RSAPublicKeySpec(
+				new BigInteger(1, jkuKey), 
+				new BigInteger(1, jkuModulus)
+		);
+		try {
+			return (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(spec);
+		} catch (Exception e) {
+			throw new RuntimeException("Cannot find key generator for RSA", e); 
+		}
+
+	}
+
+	private static byte[] pad(byte[] bs) {
+		if (bs.length == 8) {
+			return bs;
+		}
+		byte[] retval = new byte[8];
+		for (int index = 0; index < retval.length; index++) {
+			retval[index] = 0;
+		}
+		int delta = retval.length - bs.length;
+		for (int index = 0; index < bs.length; index++) {
+			retval[index+delta] = bs[index];
+		}
+		return retval;
+	}
+
 	private static List<String> getConfigPropertyAsString(Properties serviceProperties, String key) {
 		String config = serviceProperties.getProperty(key);
 		if (config == null) {
