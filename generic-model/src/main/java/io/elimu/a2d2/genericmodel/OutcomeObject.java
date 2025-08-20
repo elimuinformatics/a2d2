ae06604509fbd8ba4dbf5a98ea0596df03a89f19
Aziz Boxwala <aziz.boxwala@elimu.io>
2025-08-19 17:26:50 -0700

    RT-2260 Created OutcomeObject class (#458)
    * Created OutcomeObject class
    
    * Changed capitalization of severity enum values and removed fatal

---
+++ generic-model/src/main/java/io/elimu/a2d2/genericmodel/OutcomeObject.java
@@ -0,0 +1,52 @@
+package io.elimu.a2d2.genericmodel;
+
+import java.util.ArrayList;
+import java.util.List;
+public class OutcomeObject implements java.io.Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private List<Issue> issues;
+
+    private void init() {
+        if (issues == null) {
+            issues = new ArrayList<>();
+        }
+    }
+
+    public OutcomeObject() {
+        init();
+    }
+
+    public List<Issue> getIssues() {
+        return issues;
+    }
+
+    public void setIssues(List<Issue> issues) {
+        this.issues = issues;
+    }
+
+    public void addIssue(Issue issue) {
+        this.issues.add(issue);
+    }
+
+    public void removeIssue(Issue issue) {
+        this.issues.remove(issue);
+    }
+
+    public void clearIssues() {
+        this.issues.clear();
+    }
+
+    public boolean hasErrors() {
+        if (issues == null) {
+            return false;
+        }
+        for (Issue issue : issues) {
+            if (issue != null && Issue.IssueSeverity.ERROR.equals(issue.getSeverity())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ generic-model/src/test/java/io/elimu/a2d2/genericmodel/IssueTest.java
@@ -0,0 +1,29 @@
+package io.elimu.a2d2.genericmodel;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class IssueTest {
+
+    @Test
+    public void testSeverityGetterSetter() {
+        Issue issue = new Issue();
+        issue.setSeverity(Issue.IssueSeverity.ERROR);
+        assertEquals(Issue.IssueSeverity.ERROR, issue.getSeverity());
+    }
+
+    @Test
+    public void testCodeGetterSetter() {
+        Issue issue = new Issue();
+        issue.setCode(Issue.IssueType.INVALID_VALUE);
+        assertEquals(Issue.IssueType.INVALID_VALUE, issue.getCode());
+    }
+
+    @Test
+    public void testDiagnosticsGetterSetter() {
+        Issue issue = new Issue();
+        issue.setDiagnostics("Some diagnostics");
+        assertEquals("Some diagnostics", issue.getDiagnostics());
+    }
+}
